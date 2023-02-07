901c6e75a89d2c60518a01b6eea02b0d5b242beb
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-07 22:55:37 +0900

    Time: 5 ms (52.85%), Space: 42.6 MB (53.92%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+            return false;
+
+        char[] sArr = new char[s.length()];
+        char[] tArr = new char[t.length()];
+
+        for(int i = 0 ; i < s.length() ;i++){
+            sArr[i] = s.charAt(i);
+        }
+
+        for(int i = 0 ; i < t.length() ;i++){
+            tArr[i] = t.charAt(i);
+        }
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        if(Arrays.equals(sArr, tArr))
+            return true;
+        else
+            return false;
+    }
+}
