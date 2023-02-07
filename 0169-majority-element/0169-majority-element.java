971a5dcd627d3ee6cf296e1a2bb8466debecee63
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-07 21:56:31 +0900

    Time: 16 ms (19.42%), Space: 47.2 MB (49.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,23 +3,23 @@ import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
         int max = 0;
         int ans = 0;
+        if(nums.length == 1)
+            return nums[0];
         for(int i = 0; i < nums.length; i++) {
             if (map.get(nums[i]) != null) {
                 map.replace(nums[i], map.get(nums[i]) + 1);
+                if(max < map.get(nums[i])){
+                    max = map.get(nums[i]);
+                    ans = nums[i];
+                }
+                    
             } else {
                 map.put(nums[i],1);
-                list.add(nums[i]);
             }
         }
-        for(int i = 0; i < list.size(); i++){
-            if(map.get(list.get(i)) > max){
-             max = map.get(list.get(i));
-                 ans = list.get(i);
-                }
-        }
+
         return ans;
     }
 }
