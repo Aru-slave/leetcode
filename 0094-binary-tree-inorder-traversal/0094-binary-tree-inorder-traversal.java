9bf726d96f38064b138a52f38740dabfce2f32e7
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-08 20:57:59 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (90.38%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -16,15 +16,16 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new LinkedList<>();
-        
         dfs(root,list);
         return list;
     }
     public void dfs(TreeNode root, List<Integer> list){
-        if(root != null){
-            dfs(root.left,list);
-            list.add(root.val);
-            dfs(root.right,list);
+        if(root != null){//현재 노드가 null이 아니면 왼쪽에 노드가 있는지 확인
+            //왼쪽부터 탐색 -> inorder travel
+            dfs(root.left,list);//왼쪽에 노드가 있으면 다시 탐색
+            list.add(root.val);//이 밑의 depth에 왼쪽에 노드가 없으면 다시 루트로 돌아와서
+            //리스트에 추가
+            dfs(root.right,list);//
         }
     }
 }
