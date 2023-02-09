177210309052cf0733aa38f70a98c48a46aa51d3
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-09 22:22:08 +0900

    Time: 0 ms (100.00%), Space: 39.6 MB (93.81%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -16,14 +16,14 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return root;
-        if(root.left == null && root.right == null) return root;
-        if(root.left != null || root.right != null){
+      
+     
             TreeNode term  = root.left;
             root.left = root.right;
             root.right = term;
             invertTree(root.left);
             invertTree(root.right);
-        }
+    
 
       
         
