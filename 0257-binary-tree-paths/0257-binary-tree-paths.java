d2b41a114184cb7f337f6d9c128d5a30520ab2f0
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-12 13:32:28 +0900

    Time: 8 ms (68.42%), Space: 42.5 MB (68.54%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new LinkedList<>();
+        dfs(list,root,"");
+        return list;
+        
+    }
+    public void dfs(List<String> list, TreeNode root, String path){
+        if(root == null) return;
+       
+         if(path == "")
+            path = String.valueOf(root.val);
+        else path = path + "->" + root.val;
+         if(root.left == null && root.right == null) list.add(path);
+        dfs(list,root.left,path);
+        dfs(list,root.right,path);
+        
+    }
+}
