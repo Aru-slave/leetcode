66a1f048e982d7585e216bd59b129729244bcb26
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-13 16:27:56 +0900

    Time: 2 ms (64.79%), Space: 59.5 MB (25.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
-        int price = prices[0];
+        int min = prices[0];
         for(int i = 1; i < prices.length; i++)
         {
-            if(price > prices[i])
+            if(min > prices[i])
             {
-                price = prices[i];//작은 값이 나오면 지불 값 변경
+                min = prices[i];//작은 값이 나오면 지불 값 변경
             }
-            else if (profit < (prices[i] - price))
+            else if (profit < (prices[i] - min))
             {
-                profit = prices[i] - price; 
+                profit = prices[i] - min; 
             }
         }
         return profit;
