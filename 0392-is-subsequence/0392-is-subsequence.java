6fe01b15f8d2364a81386e510103ad4856990751
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-13 17:23:53 +0900

    Time: 1 ms (87.97%), Space: 40.4 MB (41.85%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        Queue<Character> queue = new LinkedList<>();
-        for(int i = 0; i < s.length(); i++){
-            queue.add(s.charAt(i));
-        }
-        if(queue.size() == 0) return true;
+        if(s.length() == 0) return true;
+        char[] chars = s.toCharArray();
+        int index = 0;
         for(int i = 0; i < t.length(); i++){
-            if(queue.size() != 0){
-            if(queue.peek() == t.charAt(i))
-                queue.poll();
+            if(chars[index] == t.charAt(i))
+                index++;
+            if(index == chars.length) return true;
         }
-            else break;
-        }
-        if(queue.isEmpty()) return true;
-        else return false;
+        return false;
     }
 }
