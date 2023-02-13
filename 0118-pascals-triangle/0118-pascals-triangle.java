170613d5321e2835a822ca6d09e273659c9ddc26
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-13 14:55:16 +0900

    Time: 1 ms (77.12%), Space: 42.1 MB (16.49%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -10,8 +10,12 @@ class Solution {
 
         for(int i = 1; i < numRows; i++){
             list = new LinkedList<>();
-            list.add(1);
-            for(int j = 1; j < i; j++){
+     
+            for(int j = 0; j < i; j++){
+                if(j == 0){
+                    list.add(1);
+                    continue;
+                }
             list.add(ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
             }
             list.add(1);
