0e24f9fd8975084013e024c8af61c748b6bdb8f0
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-13 16:54:02 +0900

    Time: 1 ms (99.86%), Space: 46.2 MB (92.19%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-        int dp[]=new int[n+1];
+        int dp[] = new int[n+1];
         dp[0]=0;
         int index = 0;
         int nextbit = 2;
-        for(int i=1;i<=n;i++){
+        for(int i=1; i <= n ; i++){
             if(i == nextbit){
                 index = nextbit;
                 nextbit = 2*nextbit;
-                dp[i]=1;
+                dp[i] = 1;
             }
             else{
-                dp[i]=1+dp[i-index];
+                dp[i] = 1 + dp[i-index];
             }
         }
         return dp;
