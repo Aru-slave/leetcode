7f0a03bdbbfb187c3fce567f603907fb91c325cd
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-13 13:28:28 +0900

    Time: 0 ms (100.00%), Space: 39.1 MB (51.20%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    int count = 0;
+    public int climbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int[] nums = new int[n];
+        nums[0] = 1;
+        nums[1] = 2;
+        for(int i = 2; i < nums.length; i++){
+            nums[i] = nums[i-1] + nums[i-2];
+        }
+        return nums[n-1];
+    }
+}
