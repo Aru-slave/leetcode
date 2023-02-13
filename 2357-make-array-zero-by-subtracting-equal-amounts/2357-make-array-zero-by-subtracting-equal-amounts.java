16e4929ae267dcf3fb9ac9db7c787978899f9ef4
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-13 10:08:09 +0900

    Time: 4 ms (13.62%), Space: 39.8 MB (88.73%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        if(nums.length == 0) return 0;
+        PriorityQueue<Integer> p = new PriorityQueue<>();
+        for (int i : nums) {
+            if (i != 0)
+                p.add(i);
+        }
+        while (!p.isEmpty()) {
+            int size = p.size();
+            int num = p.peek();//제일 작은 수
+            for(int i = 0; i < size; i++){
+                if(p.peek() - num == 0)
+                    p.poll();
+            }
+           
+            count++;
+        }
+       return count;
+    }
+}
