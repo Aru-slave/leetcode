b94bb3483c6431f5d280e1bfceb22c19ba0c7fdd
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-13 15:28:10 +0900

    Time: 1 ms (83.66%), Space: 40.3 MB (34.34%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,7 +1,10 @@
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
-   public List<Integer> getRow(int rowIndex) {
+    public List<Integer> getRow(int rowIndex) {
         List<Integer> list = new LinkedList<>();
-        
+
         for (int i = 0; i <= rowIndex; i++) {
             list.add(0,1);
             for(int j = 1; j < i; j ++) {
