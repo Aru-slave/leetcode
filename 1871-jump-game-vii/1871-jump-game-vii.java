3a10914b0959201040c6b01c2f1e32e0df67caba
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-14 15:33:47 +0900

    Time: 13 ms (66.60%), Space: 50 MB (47.01%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.java
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -1,15 +1,23 @@
 class Solution {
-    public boolean canReach(String s, int minJ, int maxJ) {
-        int n = s.length(), pre = 0;
-        boolean[] dp = new boolean[n];
-        dp[0] = true;
-        for (int i = 1; i < n; ++i) {
-            if (i >= minJ && dp[i - minJ])
-                pre++;
-            if (i > maxJ && dp[i - maxJ - 1])
-                pre--;
-            dp[i] = pre > 0 && s.charAt(i) == '0';
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        if(s.charAt(n-1)=='1')return false;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        int maxRange = 0;
+    
+        while(!q.isEmpty()){
+          int i = q.poll();
+          int l = Math.max(i+minJump, maxRange);
+          int r = Math.min(i + maxJump, n-1);
+          for(int x=l;x<=r;x++){
+            if(x==n-1)return true;
+            if(s.charAt(x)=='0'){
+              q.add(x);
+            }
+          }
+          maxRange = r+1;
         }
-        return dp[n - 1];
+      return false;
     }
 }
