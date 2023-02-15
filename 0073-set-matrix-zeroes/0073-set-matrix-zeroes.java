6767165671554111ab066a3b300e3c7f2fee489f
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-15 17:01:24 +0900

    Time: 1 ms (81.92%), Space: 43.6 MB (93.52%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        ArrayList<Integer> x = new ArrayList<>();
+        ArrayList<Integer>y = new ArrayList<>();
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    y.add(i);
+                    x.add(j);
+                }
+            }
+        }
+        
+        for(int i:y){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j]=0;
+            }
+        }
+        
+        for(int i:x){
+            for(int j=0;j<matrix.length;j++){
+                matrix[j][i]=0;
+            }
+        }
+}
+}
