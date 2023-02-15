332fef85cff907ddfe64b83d223e47fbe2106fb7
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-15 16:27:11 +0900

    Time: 10 ms (48.36%), Space: 45.9 MB (48.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         if (strs == null) return null;
-        Map<String, List<String>> hashMap = new HashMap<>();
-        for(int i=0; i<strs.length; i++){
+        Map<String, List<String>> map = new HashMap<>();
+        for(int i=0; i < strs.length; i++){
             char[] ch = strs[i].toCharArray();
             Arrays.sort(ch);
             String key = Arrays.toString(ch);
-            List<String> val = hashMap.get(key);
-            if (val == null) {
+            List<String> val = map.get(key);//찾아지면 기존 리스트에 추가
+            if (val == null) {//없으면 새로운 리스트 선언해서 추가
                 val = new ArrayList<>();
             }
-            val.add(strs[i]);
-            hashMap.put(key, val);
+            val.add(strs[i]);//리스트에 추가
+            map.put(key, val);
         }  
-        return new ArrayList<>(hashMap.values());
+        return new ArrayList<>(map.values());
     }
 }
