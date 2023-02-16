3910c6f29234488f668de15fe91ce2950cd88e18
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-16 16:20:11 +0900

    Time: 8 ms (77.29%), Space: 47.1 MB (72.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,12 +1,10 @@
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        Stack<int[]> merged = new Stack<>();
+        LinkedList<int[]> merged = new LinkedList<>();
         for (int[] interval : intervals) {
             // if the list of merged intervals is empty or if the current
             // interval does not overlap with the previous, simply append it.
@@ -14,17 +12,15 @@ class Solution {
                 merged.add(interval);
                 continue;
             }
-            if (merged.peek()[1] < interval[0]) {
+            if (merged.getLast()[1] < interval[0]) {
                 merged.add(interval);
             }
             // otherwise, there is overlap, so we merge the current and previous
             // intervals.
             else {
-                int[] arr = merged.pop();
-                arr[1] = Math.max(arr[1], interval[1]);
-                merged.add(arr);
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
             }
         }
-        return merged.toArray(new int[0][merged.size()]);
+        return merged.toArray(new int[1][merged.size()]);
     }
 }
