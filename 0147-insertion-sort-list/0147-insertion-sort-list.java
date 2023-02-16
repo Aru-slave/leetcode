65deaf2e4a93c7409d60d70062198e0315ac3021
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-16 17:01:27 +0900

    Time: 3 ms (89.57%), Space: 41.7 MB (86.43%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -1,18 +1,48 @@
 public class Solution {
 public ListNode insertionSortList(ListNode head) {
-    ListNode helper=new ListNode(0);
-    ListNode pre=helper;
-    ListNode current=head;
-    while(current!=null) {
-        pre=helper;
-        while(pre.next!=null&&pre.next.val<current.val) {
-            pre=pre.next;
-        }
-        ListNode next=current.next;
-        current.next=pre.next;
-        pre.next=current;
-        current=next;
+    if (head == null || head.next == null)
+    {
+        return head;
     }
-    return helper.next;
+
+    ListNode sortedHead = head, sortedTail = head;
+    head = head.next;
+    sortedHead.next = null;
+    
+    while (head != null)
+    {
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+        
+        // new val is less than the head, just insert in the front
+        if (temp.val <= sortedHead.val)
+        {
+            temp.next = sortedHead;
+            sortedTail = sortedHead.next == null ? sortedHead : sortedTail;
+            sortedHead = temp;
+        }
+        // new val is greater than the tail, just insert at the back
+        else if (temp.val >= sortedTail.val)
+        {
+            sortedTail.next = temp;
+            sortedTail = sortedTail.next;
+        }
+        // new val is somewhere in the middle, we will have to find its proper
+        // location.
+        else
+        {
+            ListNode current = sortedHead;
+            while (current.next != null && current.next.val < temp.val)
+            {
+                current = current.next;
+            }
+            
+            temp.next = current.next;
+            current.next = temp;
+        }
+    }
+    
+    return sortedHead;
 }
 }
