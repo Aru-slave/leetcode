02669f6e34433d7045f98089fdf3c68361d45833
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-16 18:10:13 +0900

    Time: 5 ms (66.38%), Space: 42.2 MB (23.63%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -5,13 +5,20 @@ class Solution {
         if(head == null || head.next == null) return head;
         List<Integer> list = new LinkedList<>();
         ListNode temp = head;
-        while(head != null){
-            list.add(head.val);
-            head = head.next;
+        while(temp != null){
+            list.add(temp.val);
+            temp = temp.next;
         }
         int[] arr = list.stream().mapToInt(Integer::intValue).toArray();
         Arrays.sort(arr);
-       return makeListNode(arr);
+        temp = head;
+        int i = 0;
+        while(temp != null){
+            temp.val = arr[i];
+            temp = temp.next;
+            i++;
+        }
+       return head;
 
     }
     public ListNode makeListNode(int[] arr){
