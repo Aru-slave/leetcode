46820d06500f03968983ab6f23b90bcc77fb0c94
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-16 17:08:54 +0900

    Time: 3 ms (89.57%), Space: 41.9 MB (52.38%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -1,48 +1,31 @@
-public class Solution {
-public ListNode insertionSortList(ListNode head) {
-    if (head == null || head.next == null)
-    {
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null) return head;
+        ListNode current = head.next;
+        ListNode pre = head;
+        while(current !=null){
+            if(current.val>=pre.val){
+                current = current.next;
+                pre = pre.next;
+            }
+            else{
+                pre.next = current.next;
+                if(current.val<=head.val){ 
+                    current.next = head;
+                    head = current;
+                }
+                else{
+                    ListNode search = head;
+                    while(search.next != null && search.next.val<current.val){
+                        search = search.next;
+                    }
+                    ListNode tmp = search.next;
+                    search.next = current;
+                    current.next = tmp;
+                }
+                current = pre.next;
+            }
+        }
         return head;
     }
-
-    ListNode sortedHead = head, sortedTail = head;
-    head = head.next;
-    sortedHead.next = null;
-    
-    while (head != null)
-    {
-        ListNode temp = head;
-        head = head.next;
-        temp.next = null;
-        
-        // new val is less than the head, just insert in the front
-        if (temp.val <= sortedHead.val)
-        {
-            temp.next = sortedHead;
-            sortedTail = sortedHead.next == null ? sortedHead : sortedTail;
-            sortedHead = temp;
-        }
-        // new val is greater than the tail, just insert at the back
-        else if (temp.val >= sortedTail.val)
-        {
-            sortedTail.next = temp;
-            sortedTail = sortedTail.next;
-        }
-        // new val is somewhere in the middle, we will have to find its proper
-        // location.
-        else
-        {
-            ListNode current = sortedHead;
-            while (current.next != null && current.next.val < temp.val)
-            {
-                current = current.next;
-            }
-            
-            temp.next = current.next;
-            current.next = temp;
-        }
-    }
-    
-    return sortedHead;
-}
 }
