22f6fd238346d75083570a24577058724f95e7ce
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-16 17:53:31 +0900

    Time: 5 ms (66.38%), Space: 41.9 MB (52.38%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -1,31 +1,27 @@
+
+
 class Solution {
     public ListNode insertionSortList(ListNode head) {
         if(head == null || head.next == null) return head;
-        ListNode current = head.next;
-        ListNode pre = head;
-        while(current !=null){
-            if(current.val >= pre.val){
-                current = current.next;
-                pre = pre.next;
-            }
-            else{
-                pre.next = current.next;
-                if(current.val <= head.val){ 
-                    current.next = head;
-                    head = current;
-                }
-                else{
-                    ListNode search = head;
-                    while(search.next != null && search.next.val < current.val){
-                        search = search.next;
-                    }
-                    ListNode tmp = search.next;
-                    search.next = current;
-                    current.next = tmp;
-                }
-                current = pre.next;
-            }
+        List<Integer> list = new LinkedList<>();
+        ListNode temp = head;
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
         }
-        return head;
+        int[] arr = list.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(arr);
+       return makeListNode(arr);
+
     }
-}
+    public ListNode makeListNode(int[] arr){
+        ListNode ans  = new ListNode();
+        ans.val = arr[0];
+        ListNode ansNext = ans;
+        for(int i = 1; i < arr.length; i++){
+            ansNext.next= new ListNode(arr[i]);
+            ansNext = ansNext.next;
+        }
+        return ans;
+    }
+    }
