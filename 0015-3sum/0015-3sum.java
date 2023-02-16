2245c6010defcfab4ec0a7730219f834dcce5545
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-16 13:38:13 +0900

    Time: 282 ms (32.43%), Space: 50.4 MB (28.74%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,24 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        //리스트에 들어가는건 같은건 못들어간다. - >set 쓰기, 중복 제거
-        //서로 다른 인덱스 3개의 숫자를 더했을 때 0이되는 조합 찾기
-        Set<List<Integer>> set = new HashSet<>();
-        Arrays.sort(nums);
-        int sum = 0;
-        for(int i = 0; i < nums.length - 2; i++){
-            int k = i+1;
-            int j = nums.length - 1;
-            sum = 0;
-            while(k < j){
-                sum = nums[i] + nums[k] + nums[j];
-                if(sum > 0) j--;
-                if(sum < 0) k++;
-                if(sum == 0) set.add(Arrays.asList(nums[i],nums[k++],nums[j--]));
+public List<List<Integer>> threeSum(int[] nums) {
+    int n=  nums.length ;
+    Arrays.sort(nums); 
+    
+    Set<List<Integer>> ans = new HashSet<>();
+    for(int i= 0 ;i<n-2 ;i++){
+            // Using Two pointers method with sorted entries 
+            int tar = -1*nums[i];
+            int s = i+1 , e = n-1;
+            while(s<e){
+                int val = nums[s]+nums[e];
+                if(val == tar){
+                    List<Integer> res = new ArrayList<>();
+                    res.add(nums[i]) ; res.add(nums[s]) ; res.add(nums[e]);
+                    ans.add(res);
+                    s++;e--;
+                }else if(val > tar) e--;
+                 else s++;
             }
-            
-        }
-        return new ArrayList<>(set);
-    }
+       }
+    return new ArrayList<>(ans);
+}
 }
