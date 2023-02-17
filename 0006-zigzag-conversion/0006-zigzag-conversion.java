b30d16c756028f0af85b6a61fd9db95e42c0f0d5
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-17 14:39:03 +0900

    Time: 12 ms (41.16%), Space: 42.9 MB (47.15%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,43 +1,45 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public String convert(String s, int numRows) {
-        Map<Integer,StringBuilder> map = new HashMap<>();
+        Map<Integer,String> map = new HashMap<>();
         boolean check = false;
         int index = 0;
+        StringBuilder sb;
         for(int i = 0; i < s.length(); i++){
             if(!check){
                 if(map.get(index) == null) {
-                    map.put(index, new StringBuilder(String.valueOf(s.charAt(i))));
+                    map.put(index,String.valueOf(s.charAt(i)));
                     index++;
                     if(index == numRows - 1) check = true;
                 }
                 else {
-                    map.put(index,map.get(index).append(s.charAt(i)));
+                    sb = new StringBuilder(map.get(index));
+                    sb.append(s.charAt(i));
+                    map.put(index,sb.toString());
                     index++;
                     if(index == numRows - 1) check = true;
                 }
             }
-            else  {
-                if(map.get(index) == null) {
-                    map.put(index, new StringBuilder(String.valueOf(s.charAt(i))));
-                    index--;
-                    if(index == 0) check = false;
-                }
-                else {
-                    map.put(index,map.get(index).append(s.charAt(i)));
-                    index--;
-                    if(index == 0) check = false;
-                }
-
+             else  {
+                    if(map.get(index) == null) {
+                        map.put(index,String.valueOf(s.charAt(i)));
+                        index--;
+                        if(index == 0) check = false;
+                    }
+                    else {
+                        sb = new StringBuilder(map.get(index));
+                        sb.append(s.charAt(i));
+                        map.put(index,sb.toString());
+                        index--;
+                        if(index == 0) check = false;
+                    }
+                
             }
-
-        }
-        StringBuilder sb = new StringBuilder(map.get(0));
+        
+    }
+    sb = new StringBuilder(map.get(0));
         for(int i = 1; i < map.size(); i++){
-            sb.append(map.get(i));
+        sb.append(map.get(i));
         }
         return sb.toString();
-    }
+}
 }
