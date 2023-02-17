084c5cd518a0a87fd9585a0ae2e0dcda76593e55
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-17 14:24:08 +0900

    Time: 8 ms (53.44%), Space: 42.7 MB (54.38%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,48 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    public String convert(String s, int numRows) {
-        Map<Integer,String> map = new HashMap<>();
-        boolean check = false;
-        int index = 0;
-        StringBuilder sb;
-        for(int i = 0; i < s.length(); i++){
-            if(!check){
-                if(map.get(index) == null) {
-                    map.put(index,String.valueOf(s.charAt(i)));
-                    index++;
-                    if(index == numRows - 1) check = true;
-                }
-                else {
-                    sb = new StringBuilder(map.get(index));
-                    sb.append(s.charAt(i));
-                    map.put(index,sb.toString());
-                    index++;
-                    if(index == numRows - 1) check = true;
-                }
-            }
-             else  {
-                    if(map.get(index) == null) {
-                        map.put(index,String.valueOf(s.charAt(i)));
-                        index--;
-                        if(index == 0) check = false;
-                    }
-                    else {
-                        sb = new StringBuilder(map.get(index));
-                        sb.append(s.charAt(i));
-                        map.put(index,sb.toString());
-                        index--;
-                        if(index == 0) check = false;
-                    }
-                
-            }
-        
+  public String convert(String s, int nRows) {
+    char[] c = s.toCharArray();
+    int len = c.length;
+    StringBuffer[] sb = new StringBuffer[nRows];
+    for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+    
+    int i = 0;
+    while (i < len) {
+        for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
+            sb[idx].append(c[i++]);
+        for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
+            sb[idx].append(c[i++]);
     }
-    sb = new StringBuilder(map.get(0));
-        for(int i = 1; i < map.size(); i++){
-        sb.append(map.get(i));
-        }
-        return sb.toString();
+    for (int idx = 1; idx < sb.length; idx++)
+        sb[0].append(sb[idx]);
+    return sb[0].toString();
 }
 }
