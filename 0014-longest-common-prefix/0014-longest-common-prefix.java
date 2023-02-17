4bdd6c72652603cd2136be29d6c1d0fe8efcab2e
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-17 09:42:12 +0900

    Time: 1 ms (74.73%), Space: 40.4 MB (61.87%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,17 +5,17 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0)
             return "";
-        
-        
+
+
         Arrays.sort(strs);
         String first = strs[0];
         String last = strs[strs.length - 1];
-        int count = 0;
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < first.length(); i++){
             if(first.charAt(i) != last.charAt(i) || i > last.length() - 1)
-                return first.substring(0,count);
-            count ++;
+                return sb.toString();
+            sb.append(first.charAt(i));
         }
-        return first.substring(0,count);
-}
+        return sb.toString();
+    }
 }
