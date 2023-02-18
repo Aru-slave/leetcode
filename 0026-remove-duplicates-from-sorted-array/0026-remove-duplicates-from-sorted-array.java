ae1ed6af2ac2e335f9acc5e2284d515f2c9bce3a
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-18 09:36:50 +0900

    Time: 1 ms (100.00%), Space: 44 MB (49.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,15 +3,15 @@ class Solution {
         if(nums.length == 0)
             return 0;
         
-        int addIndex = 1; //index that unique characters will be inserted at
+        int index = 1; 
 
         for(int i = 0; i < nums.length - 1; i++) {
             
-            if(nums[i] < nums[i + 1]){ //if true, num[i + 1] is a new unique number
-              nums[addIndex] = nums[i + 1];
-              addIndex++;
+            if(nums[i] < nums[i + 1]){
+              nums[index] = nums[i + 1];
+              index++;
             }
         }
-        return addIndex;
+        return index;
     }
 }
