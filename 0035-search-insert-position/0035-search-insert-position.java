5f8cc6ff7a66db314d0c25b3e92a6eb6c1a65e0d
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-18 10:18:03 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (93.42%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,12 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-    int i = 0;
-        while(nums[i] < target){
-            i++;
-            if(i > nums.length - 1) break;
+    public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
         }
-        return i;
-}
+        return low;
+    }
 }
