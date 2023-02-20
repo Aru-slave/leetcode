e93c4914b470a43275f06445379121f3b8ec3ac1
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 14:23:56 +0900

    Time: 19 ms (22.66%), Space: 51.8 MB (33.05%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,18 +1,18 @@
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
         ListNode temp = head;
         while(temp != null){
             list.add(temp.val);
             temp = temp.next;
         }
         int[] arr = list.stream().mapToInt(Integer::intValue).toArray();
-        Arrays.sort(arr);
+        Collections.sort(list);
         
         ListNode ans = head;
         int i = 0;
         while(ans != null){
-            ans.val = arr[i];
+            ans.val = list.get(i);
             i++;
             ans = ans.next;
         }
