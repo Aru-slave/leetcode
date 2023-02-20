8a474b25a635810bcc0b3e36354bc41f20671f78
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 10:02:30 +0900

    Time: 570 ms (75.29%), Space: 43.2 MB (27.06%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.java
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.java
@@ -1,19 +1,75 @@
 class Solution {
-    public int maxProduct(String s) {
-        return dfs(s.toCharArray(),"","",0,0);
-    }
-    
-    public int dfs(char[] arr,String s,String p,int pos,int max){
-        if(pos == arr.length){
-            if(isValid(s) && isValid(p))max = Math.max(max,s.length() * p.length());
-            return max;
-        }
-        return Math.max(dfs(arr,s + arr[pos],p,pos + 1,max),Math.max(dfs(arr,s,p + arr[pos],pos+1,max),dfs(arr,s,p,pos+1,max)));
-    }
-    
-    public boolean isValid(String s){
-        int i = 0,j = s.length()-1;
-        while(i < j)if(s.charAt(i++) != s.charAt(j--))return false;
-        return true;
-    }
+
+    //using bitmask
+   public int maxProduct(String s) {
+      
+      if(s.length() == 2) {
+         return 1;
+      }
+      
+      int n = s.length();
+      char[] ch = s.toCharArray();
+   
+      //bitmask to the length map
+      Map<Integer, Integer> bitMasktoLengthMap = new HashMap<>();
+      
+      //using bitmask , calculate all the bitmask for 1 to 2^N
+      // 1, 2, 3,............2024 ( 2 ^ 11 )
+      for(int bitmask = 1 ; bitmask< Math.pow(2, n) ;bitmask++){
+          
+          //find all the subsequence(s) in the string using bitmask
+          StringBuilder subsequence = new StringBuilder();
+          for(int j = 0; j < n; j++ ){
+   
+             //if one of the bit is set to 1, then append that character to the subsequence.
+             if(( bitmask & (1 << j ) ) != 0) { 
+                subsequence.append(ch[j]);  //constructing subsequence of different length
+             }
+          }
+          
+          //if the sequence constructed above is a palindrome,
+         // then store the mask and its length .
+          if(isPalindrome(subsequence.toString().toCharArray())){
+             bitMasktoLengthMap.put(bitmask, subsequence.length());
+          }
+      }
+      
+      Set<Integer> allBitMasks = bitMasktoLengthMap.keySet();
+      
+      int maxProduct = Integer.MIN_VALUE;
+      for( int mask1 : allBitMasks){
+         
+         for( int mask2 : allBitMasks){
+   
+            //product of lengths of 2 disjoint palindromic subsequence(s)
+            //if product of AND operation is 0 then subsequnce(s) are DIS-JOINT
+             if(( mask1 & mask2 ) == 0){
+                int product = bitMasktoLengthMap.get(mask1) * bitMasktoLengthMap.get(mask2);
+                maxProduct = Math.max(maxProduct, product);
+             }
+         }
+      }
+      
+      
+      return maxProduct;
+   }
+   
+   private boolean isPalindrome( char[] chars ) {
+      
+      int i = 0;
+      int j = chars.length - 1;
+      
+      if(i == j)
+         return true;
+      
+      while(i < j){
+         if(chars[i] != chars[j]){
+            return false;
+         }
+         i++;
+         j--;
+      }
+      
+      return true;
+   }
 }
