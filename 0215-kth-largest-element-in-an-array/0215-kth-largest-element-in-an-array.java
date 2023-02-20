5bb33cec580332340df1de7604c53b5412a9d7e5
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 16:16:52 +0900

    Time: 16 ms (86.12%), Space: 49.4 MB (99.86%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,57 +1,31 @@
 public class Solution {
-    
     public int findKthLargest(int[] nums, int k) {
-
-        return select(nums, k-1);
+        return quickSelect(nums, 0, nums.length - 1, k);
     }
     
-    // Quick select
-    private int select(int[] nums, int k) {
-        int left = 0, right = nums.length-1;
-        while(true) {
-            if(left == right)
-                return nums[left];
-            int pivotIndex = medianOf3(nums, left, right);
-            pivotIndex = partition(nums, left, right, pivotIndex);
-            if(pivotIndex == k)
-                return nums[k];
-            else if(pivotIndex > k)
-                right = pivotIndex-1;
-            else
-                left = pivotIndex+1;
-        }
-    }
-    
-    //Use median-of-three strategy to choose pivot
-    private int medianOf3(int[] nums, int left, int right) {
-        int mid = left + (right - left) / 2;
-        if(nums[right] > nums[left])
-            swap(nums, left, right);
-        if(nums[right] > nums[mid])
-            swap(nums, right, mid);
-        if(nums[mid] > nums[left])
-            swap(nums,left, mid);
-        return mid;
-    }
-    
-    private int partition(int[] nums, int left, int right, int pivotIndex) {
-        int pivotValue = nums[pivotIndex];
-        swap(nums, pivotIndex, right);
-        int index = left;
-        for(int i = left; i < right; ++i) {
-            if(nums[i] > pivotValue) {
-                swap(nums, index, i);
-                ++index;
+    private int quickSelect(int[] nums, int start, int end, int k) {
+        int pivot = end;
+        int left = start;
+        int right = end - 1;
+        while (left <= right) {
+            if (nums[left] > nums[pivot]) {
+                swap(nums, left, right);
+                right--;
+            } else {
+                left++;
             }
         }
-        swap(nums, right, index);
-        return index;
+        swap(nums, left, pivot);
+        
+        int rank = nums.length - left;
+        if (rank == k) return nums[left];
+        if (rank > k) return quickSelect(nums, left + 1, end, k);
+        return quickSelect(nums, start, left - 1, k);
     }
     
     private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
+        int tmp = nums[a];
         nums[a] = nums[b];
-        nums[b] = temp;
+        nums[b] = tmp;
     }
-
 }
