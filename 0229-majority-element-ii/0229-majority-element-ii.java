6d18afcf69f131976b047754e0e0d4c53e3cf005
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 17:14:55 +0900

    Time: 2 ms (71.98%), Space: 46.1 MB (58.28%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,30 +1,35 @@
 
 
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
-        int count = nums.length / 3;
-        Map<Integer,Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        if(nums.length == 1) {
-            list.add(nums[0]);
-            return list;
-            }
-        for(int i = 0,j = nums.length - 1; i <= j; i++,j--){
-            if(map.get(nums[i]) == null){
-                map.put(nums[i],1);
-            }
-            else map.put(nums[i],map.get(nums[i])+1);
-        if(i != j){
-        if(map.get(nums[j]) == null){
-            map.put(nums[j],1);
-        }
-        else map.put(nums[j],map.get(nums[j])+1);
-    }
-        }
-        for(int i : map.keySet()){
-            if(map.get(i) > count)
-                list.add(i);
-        }
-        return list;
-    }
+public List<Integer> majorityElement(int[] a) {
+  // we can only have maximum 2 majority elements
+  int n = a.length;
+  int c1 = 0, c2 = 0;
+  Integer m1 = null, m2 = null;
+  
+  // step 1. find out those 2 majority elements
+  // using Moore majority voting algorithm
+  for (int i = 0; i < n; i++) {
+    if (m1 != null && a[i] == m1)      { c1++; } 
+    else if (m2 != null && a[i] == m2) { c2++; }
+    else if (c1 == 0)                  { m1 = a[i]; c1 = 1; }
+    else if (c2 == 0)                  { m2 = a[i]; c2 = 1; } 
+    else                               { c1--; c2--; }
+  }
+  
+  // step 2. double check
+  c1 = 0; c2 = 0;
+  
+  for (int i = 0; i < n; i++) {
+    if (m1 != null && a[i] == m1) c1++;
+    if (m2 != null && a[i] == m2) c2++;
+  }
+  
+  List<Integer> res = new ArrayList<Integer>();
+  
+  if (c1 > n / 3) res.add(m1);
+  if (c2 > n / 3) res.add(m2);
+  
+  return res;
+}
 }
