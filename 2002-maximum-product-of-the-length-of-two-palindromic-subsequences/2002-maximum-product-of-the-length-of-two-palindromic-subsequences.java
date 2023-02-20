6d03ce3e513fc7ac4fe1e502aead6bea7faad109
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 10:13:56 +0900

    Time: 2021 ms (34.11%), Space: 42.7 MB (60.78%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.java
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.java
@@ -1,75 +1,40 @@
 class Solution {
-
-    //using bitmask
-   public int maxProduct(String s) {
-      
-      if(s.length() == 2) {
-         return 1;
-      }
-      
-      int n = s.length();
-      char[] ch = s.toCharArray();
-   
-      //bitmask to the length map
-      Map<Integer, Integer> bitMasktoLengthMap = new HashMap<>();
-      
-      //using bitmask , calculate all the bitmask for 1 to 2^N
-      // 1, 2, 3,............2024 ( 2 ^ 11 )
-      for(int bitmask = 1 ; bitmask< Math.pow(2, n) ;bitmask++){
-          
-          //find all the subsequence(s) in the string using bitmask
-          StringBuilder subsequence = new StringBuilder();
-          for(int j = 0; j < n; j++ ){
-   
-             //if one of the bit is set to 1, then append that character to the subsequence.
-             if(( bitmask & (1 << j ) ) != 0) { 
-                subsequence.append(ch[j]);  //constructing subsequence of different length
-             }
-          }
-          
-          //if the sequence constructed above is a palindrome,
-         // then store the mask and its length .
-          if(isPalindrome(subsequence.toString().toCharArray())){
-             bitMasktoLengthMap.put(bitmask, subsequence.length());
-          }
-      }
-      
-      Set<Integer> allBitMasks = bitMasktoLengthMap.keySet();
-      
-      int maxProduct = Integer.MIN_VALUE;
-      for( int mask1 : allBitMasks){
-         
-         for( int mask2 : allBitMasks){
-   
-            //product of lengths of 2 disjoint palindromic subsequence(s)
-            //if product of AND operation is 0 then subsequnce(s) are DIS-JOINT
-             if(( mask1 & mask2 ) == 0){
-                int product = bitMasktoLengthMap.get(mask1) * bitMasktoLengthMap.get(mask2);
-                maxProduct = Math.max(maxProduct, product);
-             }
-         }
-      }
-      
-      
-      return maxProduct;
-   }
-   
-   private boolean isPalindrome( char[] chars ) {
-      
-      int i = 0;
-      int j = chars.length - 1;
-      
-      if(i == j)
-         return true;
-      
-      while(i < j){
-         if(chars[i] != chars[j]){
-            return false;
-         }
-         i++;
-         j--;
-      }
-      
-      return true;
-   }
+    
+    int max = 0;
+    public int maxProduct(String s) {
+        
+        char[] c = s.toCharArray();
+        dfs(c, 0, "", "");
+        
+        return max;
+    }
+    
+    public void dfs(char[] c, int i, String s1, String s2){
+        
+        if(i >= c.length){
+            
+            if(isVaild(s1) && isVaild(s2))
+                max = Math.max(max, s1.length()*s2.length());
+            return;
+        }
+        
+        dfs(c, i+1, s1+c[i], s2);
+        dfs(c, i+1, s1, s2+c[i]);
+        dfs(c, i+1, s1, s2);
+    }
+    
+     public boolean isVaild(String str){
+ 
+        int i = 0, j = str.length() - 1;
+ 
+        while (i < j) {
+ 
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+ 
+        return true;
+    }
 }
