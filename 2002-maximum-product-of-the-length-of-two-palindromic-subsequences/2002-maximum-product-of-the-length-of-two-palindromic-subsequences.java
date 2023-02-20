80403db7d5e2204b79404d9cc0f57d5e2ad73b8f
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 10:37:00 +0900

    Time: 2286 ms (12.93%), Space: 42.4 MB (82.35%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.java
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.java
@@ -1,44 +1,40 @@
 class Solution {
+    
+    int max = 0;
     public int maxProduct(String s) {
-        HashMap<Integer,Integer> map=new HashMap<>(); // KEY=bit mask , VALUE= length of the string generated from that mask
-        int n=s.length();
-        for(int mask=0;mask<(1<<n);mask++){// generate bitmask from 1 to 2^n 
-            String temp="";
-            for(int i=0;i<n;i++){ 
-                if((mask & (1<<i)) !=0) // generate the string from the mask 
-                    temp+=s.charAt(i);
-            }
-            
-            if(isPali(temp)){ // check if its a palindrome
-                map.put(mask,temp.length()); 
-            }
-        }
         
-        int res=0;
-        for(int i: map.keySet()){
-            for(int j :map.keySet()){
-                if((i&j)==0){ // if AND of two bitmask is zero means they are disjoint
-                    res=Math.max(res, map.get(i)*map.get(j));
-                }
-            }
-        }
+        char[] c = s.toCharArray();
+        dfs(c, 0, "", "");
         
-        return res;
-        
-        
-        
-        
+        return max;
     }
     
-    private boolean isPali(String s){
-        int i=0;
-        int j=s.length()-1;
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j))
+    public void dfs(char[] c, int i, String s1, String s2){
+        
+        if(i >= c.length){
+            
+            if(isPalin(s1) && isPalin(s2))
+                max = Math.max(max, s1.length()*s2.length());
+            return;
+        }
+        
+        dfs(c, i+1, s1+c[i], s2);
+        dfs(c, i+1, s1, s2+c[i]);
+        dfs(c, i+1, s1, s2);
+    }
+    
+     public boolean isPalin(String str){
+ 
+        int i = 0, j = str.length() - 1;
+ 
+        while (i < j) {
+ 
+            if (str.charAt(i) != str.charAt(j))
                 return false;
             i++;
             j--;
         }
+ 
         return true;
     }
 }
