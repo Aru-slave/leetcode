356a08336173a7f4b3cfcef1d41ef5cfa45b2e64
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 15:18:43 +0900

    Time: 5 ms (97.82%), Space: 42.3 MB (31.49%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,27 +1,35 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 class Solution {
-    public String largestNumber(int[] nums) {
-        List<String> list = new ArrayList<>(nums.length);
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+           return order2.compareTo(order1);
+        }
+    }
 
-        for(int i = 0; i < nums.length; i++){
-            list.add(String.valueOf(nums[i]));
+    public String largestNumber(int[] nums) {
+        // Get input integers as strings.
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
         }
-        list.sort((a, b) -> {
-            String exam1 = a + b;
-            String exam2 = b + a;
-            if (Long.parseLong(exam2) > Long.parseLong(exam1)) return 1;
-            else return -1;
-        });
-    if (list.get(0).charAt(0) == '0') {
-        return "0";
-    }
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < list.size(); i++){
-                sb.append(list.get(i));
-            }
-            return sb.toString();
+
+        // Sort strings according to custom comparator.
+        Arrays.sort(asStrs, new LargerNumberComparator());
+
+        // If, after being sorted, the largest number is `0`, the entire number
+        // is zero.
+        if (asStrs[0].equals("0")) {
+            return "0";
         }
+
+        // Build largest number from sorted array.
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+
+        return largestNumberStr;
     }
+}
