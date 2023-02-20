77dcea860980524d6f31c0d2a78603ee8e29aa6c
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 14:04:55 +0900

    Time: 1 ms (99.63%), Space: 41.5 MB (96.70%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(j!=i && arr[i]==arr[j]*2) {
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                if(i != j && arr[i] == arr[j] * 2)
                     return true;
-                }
             }
         }
         return false;
-    }
+}
 }
