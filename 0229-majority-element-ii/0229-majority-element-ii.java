7397601ec361045b7dfba1f94ed9538865371a6a
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-20 17:08:35 +0900

    Time: 13 ms (18.61%), Space: 45.6 MB (84.16%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,14 +1,25 @@
 
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int count = nums.length / 3;
         Map<Integer,Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
+        if(nums.length == 1) {
+            list.add(nums[0]);
+            return list;
+            }
+        for(int i = 0,j = nums.length - 1; i <= j; i++,j--){
             if(map.get(nums[i]) == null){
                 map.put(nums[i],1);
             }
             else map.put(nums[i],map.get(nums[i])+1);
+        if(i != j){
+        if(map.get(nums[j]) == null){
+            map.put(nums[j],1);
+        }
+        else map.put(nums[j],map.get(nums[j])+1);
+    }
         }
         for(int i : map.keySet()){
             if(map.get(i) > count)
