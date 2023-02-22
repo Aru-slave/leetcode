237ea97c4af3629b95858f9e37189ebb84f98e61
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-22 10:27:51 +0900

    Time: 13 ms (9.53%), Space: 45.6 MB (6.24%) - LeetHub

--- 0563-binary-tree-tilt/0563-binary-tree-tilt.java
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.java
@@ -1,26 +1,13 @@
 class Solution {
-    int sum=0;
-
+    int sum =0;
     public int findTilt(TreeNode root) {
-        if(root==null)
-            return 0;
-        
-        dfs(root);
-        
-        return sum;
+        if(root == null) return 0;
+     return Math.abs( findSum(root.left)- findSum(root.right))+ findTilt(root.left)+findTilt(root.right);
     }
     
-    private int dfs(TreeNode root){
-        if(root == null)
-            return 0;
-        
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        
-        sum = sum + Math.abs(left-right);
-        
-        root.val = root.val + left + right;  
-        
-        return root.val;
-    }
+   int findSum(TreeNode root){
+       if(root==null) return 0;
+       return findSum(root.left )+findSum(root.right)+root.val;
+   }
+  
 }
