7cfe7dd44fe92c72dbccce6af3a2bb7454f0f94a
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-22 09:34:15 +0900

    Time: 1 ms (34.97%), Space: 43.9 MB (9.77%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
@@ -1,24 +1,12 @@
 class Solution {
- public int maxDepth(Node root) {
- if(root == null) return 0;
- Stack<Node> stack = new Stack<>();
- Stack<Integer> num = new Stack<>();
- stack.push(root);
- num.push(1);
- int depth = 1;
-
- while (!stack.empty()) {
-  Node node = stack.pop();
-  int count = num.pop();
-
-  if (node.children != null) {
-   for (Node nod : node.children) {
-     stack.push(nod);
-     depth = Math.max(count + 1, depth);
-     num.push(count + 1);
-   }
-  }
- }
-
- return depth;
- } }
+    public int maxDepth(Node root) {
+         if(root==null) return 0;
+         
+         int max=0;
+      
+          for(Node c: root.children){
+              max = Math.max(max, maxDepth(c));
+              }
+        return 1+max;
+    }
+}
