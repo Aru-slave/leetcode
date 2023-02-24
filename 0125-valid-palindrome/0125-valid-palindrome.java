013ca85344d642e5a56d773957d5a0fc3325b168
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-24 23:24:33 +0900

    Time: 772 ms (8.55%), Space: 43.6 MB (21.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,12 +3,7 @@ class Solution {
         String temp = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
         int i = 0;
         int j = temp.length()-1;
-        while(i < j){
-            if(temp.charAt(i) != temp.charAt(j))
-                return false;
-            i++;
-            j--;
-        }
-        return true;
+        String reverse = new StringBuilder(temp).reverse().toString();
+        return temp.equals(reverse);
     }
 }
