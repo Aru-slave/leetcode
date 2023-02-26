7bf1b4c2d62b50dde1e3a4b6e940b507afdf1862
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-02-27 05:40:01 +0900

    Time: 2 ms (32.97%), Space: 40.3 MB (19.83%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,17 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-       int start=1, end=x,floorRes=0;
-       while(start<=end){
-           int mid=start+(end-start)/2;
-
-           if(mid<=x/mid){
-               floorRes=mid;
-               start=mid+1;
-           }
-           else{
-               end=mid-1;
-           }
-       } 
-       return floorRes;
+    long start = 1;
+    long end = x;
+    long mid = (start + end) / 2;
+    while(start <= end){
+        
+        if(mid*mid == x)
+           break;
+        else if(mid*mid > x){
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+        mid = (start + end) / 2;
+        
     }
+        return (int)mid;
+}
 }
