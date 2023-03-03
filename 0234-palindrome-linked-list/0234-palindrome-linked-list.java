a243db7507c2923132b317d2722486d72ccbc62a
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-03-04 08:00:53 +0900

    Time: 11 ms (32.75%), Space: 54.9 MB (73.36%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,26 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
         List<Integer> list = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        int index = 0;
         while(head != null){
             list.add(head.val);
-            stack.add(head.val);
             head = head.next;
         }
-        while(!stack.isEmpty()){
-            if(stack.pop() != list.get(index)) return false;
-            index++;
+        for(int i = list.size() - 1, j = 0; i >=list.size()/2; i--,j++){
+            if(list.get(i) != list.get(j)) return false;
+             
         }
         return true;
     }
