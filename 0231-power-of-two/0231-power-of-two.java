44aae6829f8bcc3c4b5540ac1b44aefb5ddb3f69
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-03-04 07:45:29 +0900

    Time: 1 ms (100.00%), Space: 40.1 MB (18.26%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int count = 0;
+        while(n != 0){
+            if(n%2 == 1) count++;
+            n = n /2;
+        }
+        return count == 1;
+    }
+}
