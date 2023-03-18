4a4fb44299359726b8b9e4e306e0df1309671bf3
Aru-slave <108045715+Aru-slave@users.noreply.github.com>
2023-03-18 12:42:46 +0900

    Time: 0 ms (100.00%), Space: 39.5 MB (39.66%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.java
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -1,8 +1,8 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int timeTakenForOnePass=n-1;
-        int totalPasses=time/timeTakenForOnePass;
-        int rem=time%timeTakenForOnePass;
-        return totalPasses%2==0?rem+1:n-rem;
+        int timeTakenForOnePass = n - 1;
+        int totalPasses = time / timeTakenForOnePass;
+        int rem=time % timeTakenForOnePass;
+        return totalPasses % 2 == 0 ? rem + 1 : n - rem;
     }
 }
